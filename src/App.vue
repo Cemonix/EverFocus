b8f302caebedebcea94b8cb1b08bcade4c876105
Milan Gittler <gittlermilan@seznam.cz>
2025-03-09 18:17:52 +0100

    Refactor App.vue layout to include router links and improve styling

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,53 @@
 <script setup lang="ts">
-const message = "EverFocus Extension"
 </script>
 
 <template>
   <div class="container">
-    <img src="./assets/logo_128.svg" class="logo" alt="EverFocus logo" />
-    <h1>{{ message }}</h1>
-    <h2>Hello world from Extension</h2>
+    <h1>EverFocus</h1>
+    <main>
+      <RouterView/>
+    </main>
+    <nav class="navbar">
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/block">Block</RouterLink>
+    </nav>
   </div>
 </template>
 
 <style scoped>
 .container {
-  width: 300px;
-  padding: 16px;
+  height: 100%;
+  width: 100%;
+}
+
+h1 {
+  padding: 5px;
   text-align: center;
+  font-size: 1.5rem;
+  color: #fff;
+  background-image: linear-gradient(to right, #850bb5, #1273e9);
+  text-shadow: 1px 1px 2px #000;
 }
 
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+.navbar {
+  display: flex;
+  justify-content: center;
+  align-items: end;
+  gap: 1rem;
+  margin-top: 5px;
 }
 
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+.navbar a {
+  text-decoration: none;
+  color: #000;
+  border: 1px solid #000;
+  padding: 5px 10px;
+  border-radius: 10px;
+}
+
+.navbar a:hover {
+  background-color: #1669d5;
+  color: #fff;
+  transition: 0.3s;
 }
 </style>
