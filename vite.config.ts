1598a3210defacdfa0da69f7c958060944ce635b
Milan Gittler <gittlermilan@seznam.cz>
2025-03-09 14:19:31 +0100

    Update TypeScript configuration and Vite build settings for improved module handling and output

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [vue()],
-})
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        background: 'src/background.ts',
+      },
+      output: {
+        entryFileNames: chunk => {
+          return chunk.name === 'background' ? '[name].js' : 'assets/[name].[hash].js'
+        }
+      }
+    }
+  }
+});
