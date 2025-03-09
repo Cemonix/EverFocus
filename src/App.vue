75f1a38bae90e07f05ea3827c07570e579fe1220
Milan Gittler <gittlermilan@seznam.cz>
2025-03-09 14:19:48 +0100

    Update index.html and App.vue for EverFocus branding and layout

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,30 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+const message = "EverFocus Extension"
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="container">
+    <img src="./assets/logo_128.svg" class="logo" alt="EverFocus logo" />
+    <h1>{{ message }}</h1>
+    <h2>Hello world from Extension</h2>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
+.container {
+  width: 300px;
+  padding: 16px;
+  text-align: center;
+}
+
 .logo {
   height: 6em;
   padding: 1.5em;
   will-change: filter;
   transition: filter 300ms;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
 }
 </style>
