4f44379ece2a2645dabbbe24f4767763f013e57b
Milan Gittler <gittlermilan@seznam.cz>
2025-03-11 17:44:34 +0100

    Enhance App.vue layout with gradient background and improved styling

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,9 @@
 
 <template>
   <div class="container">
-    <h1>EverFocus</h1>
+	<div class="gradient">
+		<h1>EverFocus</h1>
+	</div>
     <main>
       <RouterView/>
     </main>
@@ -15,39 +17,50 @@
 </template>
 
 <style scoped>
-.container {
-  height: 100%;
-  width: 100%;
-}
+	.container {
+		height: 100%;
+		width: 100%;
+		display: grid;
+		grid-template-rows: 1fr 4fr 1fr;
+		gap: 1rem;
+		padding: 1rem;
+		background-color: #f0f0f0;
+	}
 
-h1 {
-  padding: 5px;
-  text-align: center;
-  font-size: 1.5rem;
-  color: #fff;
-  background-image: linear-gradient(to right, #850bb5, #1273e9);
-  text-shadow: 1px 1px 2px #000;
-}
+	.gradient {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		background-image: linear-gradient(to right, #850bb5, #1273e9);
+		padding: 5px;
+	}
 
-.navbar {
-  display: flex;
-  justify-content: center;
-  align-items: end;
-  gap: 1rem;
-  margin-top: 5px;
-}
+	h1 {
+		text-align: center;
+		font-size: 1.5rem;
+		color: #fff;
+		text-shadow: 1px 1px 2px #000;
+	}
 
-.navbar a {
-  text-decoration: none;
-  color: #000;
-  border: 1px solid #000;
-  padding: 5px 10px;
-  border-radius: 10px;
-}
+	.navbar {
+		display: flex;
+		justify-content: center;
+		align-items: end;
+		gap: 1rem;
+		margin-top: 5px;
+	}
 
-.navbar a:hover {
-  background-color: #1669d5;
-  color: #fff;
-  transition: 0.3s;
-}
+	.navbar a {
+		text-decoration: none;
+		color: #000;
+		border: 1px solid #000;
+		padding: 5px 10px;
+		border-radius: 10px;
+	}
+
+	.navbar a:hover {
+		background-color: #1669d5;
+		color: #fff;
+		transition: 0.3s;
+	}
 </style>
