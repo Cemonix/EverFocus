71a59aebbb71c02a248c48e6df24b70bb1e2575c
Milan Gittler <gittlermilan@seznam.cz>
2025-03-11 17:44:58 +0100

    Add HomePage component to display blocked pages in a table format

---
+++ src/Pages/HomePage.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import TableRow from '../components/TableRow.vue';
+
+const blockedPages = ref<string[]>(['hello', 'world', 'foo', 'bar']);
+</script>
+
+<template>
+	<div class="table-container">
+		<table>
+			<tbody>
+				<TableRow v-for="page in blockedPages" :blockedPage="page" />
+			</tbody>
+		</table>
+	</div>
+</template>
+
+<style scoped>
+	.table-container {
+		max-height: 55vh;
+		display: flex;
+		justify-content: center;
+		overflow-y: auto;
+		padding: 0.5rem;
+		scrollbar-width: thin;
+    	scrollbar-color: #696969 #b7b7b7 ;
+	}
+
+	table {
+		width: 100%;
+		border-collapse: separate;
+		border-spacing: 0 0.5em;
+	}
+</style>
